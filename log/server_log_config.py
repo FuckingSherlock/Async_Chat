65cefb375c9d928b91f6f95576f10a1b56988eb0
FuckingSherlock <91081368+FuckingSherlock@users.noreply.github.com>
2023-08-25 20:02:40 +0300

    Add logger

---
+++ log/server_log_config.py
@@ -0,0 +1,9 @@
+import logging as lg
+from datetime import date
+
+lg.basicConfig(
+    filename=str(date.today())+'_server.log',
+    encoding='utf-8',
+    format="%(asctime)s %(levelname)-10s %(module)s %(message)s",
+    level=lg.DEBUG
+)
